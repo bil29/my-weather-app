d1d0aed81f6767e0cd28d41213fa49eb42abd673
Beate L <beatebl34@gmail.com>
2022-03-15 07:25:31 -0400

    updated api url

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function displayWeatherCondition(response) {
 
 function searchCity(city) {
   let apiKey = "78ed9009ce6694dc34794d4f61f7313b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
 
