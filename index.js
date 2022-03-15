3a41936faef5bdc1daa91a7bc2bdaa4394319bf4
Beate L <beatebl34@gmail.com>
2022-03-15 14:42:41 -0400

    updated api keys

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "78ed9009ce6694dc34794d4f61f7313b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${APIkey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -75,7 +75,7 @@ function displayWeatherCondition(response) {
 
 function searchCity(city) {
   let apiKey = "78ed9009ce6694dc34794d4f61f7313b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
 
